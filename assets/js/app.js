e650e583cd6b887305fc068556ff89f82041971b
Daniel <dvallejosanchez@gmail.com>
2022-11-09 20:22:14 +0100

    desplegable

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,7 @@ menu.addEventListener("click" , (e) => {
     alert.classList.toggle("dismissible")
     
 } )
+//FUNCIÓN BOTÓN CLOSE
+document.querySelector(".btn__close").addEventListener("click", () => {
+    document.querySelector("alert dismissible").classList.toggle("open");
+  });
