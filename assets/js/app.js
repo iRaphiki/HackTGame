838ab2facab13b82f65fce513c51af7cab259d3c
FranAlonso91 <franciscojalonso91@gmail.com>
2022-11-09 10:40:09 +0100

    in progress with login menu

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.getElementsByClassName("btn_close").onclick = function () {
+      this.parentNode.parentNode.remove();
+      return false;
+    };
+  };
+  
