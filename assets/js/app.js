4ae1d572320257523b3ffbade3a0e52423e89065
Daniel <dvallejosanchez@gmail.com>
2022-11-10 10:20:25 +0100

    buttons-ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,13 @@
-let alert = document.querySelector(".alert")
-let menu = document.querySelector(".hamburger")
-menu.addEventListener("click" , (e) => {
-    alert.classList.toggle("dismissible")
-    
-} )
+let alert = document.querySelector(".alert");
+let menu = document.querySelector(".hamburger");
+let close = document.querySelector(".btn__close");
+menu.addEventListener("click", (e) => {
+  alert.classList.remove("dismissible");
+});
+close.addEventListener("click", (e) => {
+  alert.classList.add("dismissible")
+});
 //FUNCIÓN BOTÓN CLOSE
 document.querySelector(".btn__close").addEventListener("click", () => {
-    document.querySelector("alert dismissible").classList.toggle("open");
-  });
+  document.querySelector("alert dismissible").classList.toggle("open");
+});
