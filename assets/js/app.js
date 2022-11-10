91cd072ccad2f0a039096faf0bc9bf368d579757
Daniel <dvallejosanchez@gmail.com>
2022-11-10 13:45:29 +0100

    ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,24 @@
 let alert = document.querySelector(".alert");
 let menu = document.querySelector(".hamburger");
-let close = document.querySelector(".btn__close");
-menu.addEventListener("click", (e) => {
-  alert.classList.remove("dismissible");
-});
-close.addEventListener("click", (e) => {
-  alert.classList.add("dismissible")
-});
-//FUNCIÓN BOTÓN CLOSE
-document.querySelector(".btn__close").addEventListener("click", () => {
-  document.querySelector("alert dismissible").classList.toggle("open");
-});
+//let clsbtn = document.querySelector(".btn__close");
+
+// menu.addEventListener("click", (e) => {
+//   alert.classList.remove("dismissible");
+// });
+// clsbtn.addEventListener("click", (e) => {
+//   alert.classList.add("dismissible");
+// });
+// //  para scroll
+ let lastScrollTop;
+ navbar = document.getElementById("navbar");
+ window.addEventListener("scroll", function () {
+   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+   if (scrollTop > lastScrollTop) {
+     navbar.style.top = "-100px";
+   } else {
+     navbar.style.top = "0";
+   }
+   lastScrollTop = scrollTop;
+ });
+
+// console.log(window.pageYOffset);
