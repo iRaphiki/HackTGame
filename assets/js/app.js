15139e7ba3e4ee4b02153f1d89d96b99d59d4d0f
Veronika Sanchez Camacho <vsc1972@gmail.com>
2022-11-13 13:54:08 +0100

    clean js

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,45 +34,7 @@ window.addEventListener("scroll", function () {
   lastScrollTop = scrollTop;
 });
 
-/* ------------------MODAL-BOX---------------------------- */
-// Open and close modal/login window
-/*
-const loginWindow = () => {
-	window.addEventListener('load', () => {
-		const openModalLogin = document.getElementById('login');
-		const openModalSignup = document.getElementById('signup');
-		const modalWindowLogin = document.getElementById('login-box');
-		const modalWindowSign = document.getElementById('signup-box');
 
-		const loginShowModalWindow = () => {
-			modalWindowLogin.style.display = 'block';
-		};
-		const signupShowModalWindow = () => {
-			modalWindowSign.style.display = 'block';
-		};
-
-		openModalLogin.addEventListener('click', loginShowModalWindow);
-		openModalSignup.addEventListener('click', signupShowModalWindow);
-
-		const closeModalButton = document.getElementById('close-modal');
-		const closeModalButtonSign = document.getElementById('close-sign');
-
-		const hideModalWindow = () => {
-			modalWindowLogin.style.display = 'none';
-			modalWindowSign.style.display = 'none';
-		};
-		closeModalButton.addEventListener('click', hideModalWindow);
-		closeModalButtonSign.addEventListener('click', hideModalWindow);
-	});
-};
-loginWindow();
-*/
-/*
-//FUNCIÓN BOTÓN CLOSE
-document.querySelector('.btn__close').addEventListener('click', () => {
-	document.querySelector('.menu_container').classList.toggle('open');
-});
-*/
 // ----- Slider new:
 function manualSlideShow() {
 
@@ -125,9 +87,7 @@ const loginWindow = () => {
     openModalSignup.forEach((element) => {
       element.addEventListener("click", signupShowModalWindow);
     });
-    //openModalLogin.addEventListener('click', loginShowModalWindow);
-    //openModalSignup.addEventListener('click', signupShowModalWindow);
-
+    
     const closeModalButton = document.getElementById("close-modal");
     const closeModalButtonSign = document.getElementById("close-sign");
 
